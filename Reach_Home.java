20671a7e50b7758368b2c5427f231a24d1e8c80d
22MH1A05L9 <127473153+22MH1A05L9@users.noreply.github.com>
2024-02-12 21:00:33 +0530

    import java.util.Scanner;     public class pavan{         public static void main(String[] args){             Scanner sc = new Scanner(System.in);             int x = sc.nextInt();             int y = sc.nextInt();             if((5*x)>=y){                 System.out.println("YES");             }             else{                 System.out.println("NO");             }
        }
    }

---
+++ Reach_Home.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    public class pavan{
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if((5*x)>=y){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            
+    }
+}
