fb92e942d7e24f4143247acf1f555b7a67c6351b
22MH1A05L9 <127473153+22MH1A05L9@users.noreply.github.com>
2024-02-12 20:54:09 +0530

    import java.util.Scanner; public  class height{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int i = sc.nextInt();         int a = sc.nextInt();         if(i>a){             System.out.println(i);         }             else{                 System.out.println(a);             }     } }

---
+++ Height.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public  class height{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int a = sc.nextInt();
+        if(i>a){
+            System.out.println(i);
+        }
+            else{
+                System.out.println(a);
+            }
+    }
+}
