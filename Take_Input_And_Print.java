7a4cfce2a58f9fb88075247f196e9d1f125a5326
22MH1A05L9 <127473153+22MH1A05L9@users.noreply.github.com>
2024-02-14 10:26:49 +0530

    import java.util.Scanner; public class nav{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class nav{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
