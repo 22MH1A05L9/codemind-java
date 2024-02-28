e7b2f5f2ed2466e4c64dcb37a8074e4ca93926ec
22MH1A05L9 <127473153+22MH1A05L9@users.noreply.github.com>
2024-02-28 10:33:07 +0530

    import java.util.Scanner; public class donation{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a =sc.nextInt();         int b =sc.nextInt();         int ans = b-a;         System.out.println(ans);     } }

---
+++ Donation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class donation{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int ans = b-a;
+        System.out.println(ans);
+    }
+}
