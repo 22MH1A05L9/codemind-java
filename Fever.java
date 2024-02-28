1bf10ef978d56de6bae785ae57be7660ce7f0015
22MH1A05L9 <127473153+22MH1A05L9@users.noreply.github.com>
2024-02-28 09:51:48 +0530

    import java.util.Scanner; public class pavan{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         if(a>98){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Fever.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class pavan{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>98){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
